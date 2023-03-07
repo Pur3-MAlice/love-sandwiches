e0a5478987cd027a37776bdfe4023fce032f14a4
Pur3-MAlice <aliceford98@live.co.uk>
2023-03-07 21:34:43 +0000

    testing API function

--- try.py
+++ try.py
@@ -14,38 +14,40 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
-    '''
-    Get sales data from the user. run while loop to ensure correct data input
-    data must be 6 int seperated by commas, while loop will run until valid.
-    '''
+    """
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, seperated by commas.")
-        print("Example: 12,34,45,63,67,45\n")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
-        validate_data(sales_data)
 
         if validate_data(sales_data):
-            print("Data is Valid!\n")
+            print("Data is valid!")
             break
 
     return sales_data
 
 
 def validate_data(values):
-    '''
-    Inside the try, converts all string into integers.
-    Raises Error if string cant be converted, or if not 6 ints
-    '''
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-                )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
@@ -54,13 +56,13 @@ def validate_data(values):
 
 
 def update_sales_worksheet(data):
-    '''
-    Update worksheet with new data provided on new row.
-    '''
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales workshet updated!\n")
+    print("Sales worksheet updated successfully.\n")
 
 
 data = get_sales_data()
